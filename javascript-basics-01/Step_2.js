ba2f376a7781d148167e49532140d83ce2fb67ef
louai <louaibaghdadi27@gmail.com>
2023-08-30 10:20:01 +0300

    base1step2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,10 @@
+var name="Louai";
+var surname="Baghdadi";
+var city="tripoli";
+
+function show(){
+    alert("Nom : "+ name + "\nPrenon : " + surname + "\ncity : " + city);
+    console.log(name)
+}
+
+show();
