9ce8f3c11cbb0b1d785e38d8ac183f619dff7e43
louai <louaibaghdadi27@gmail.com>
2023-08-30 22:04:59 +0300

    base2step5

--- javascript-basics-02/step 5/step5.js
+++ javascript-basics-02/step 5/step5.js
@@ -1,21 +1,30 @@
 
 
-function mouseOver1(){
-    document.querySelector("#image1").src="images/image1_2.jpg"
-}
+// function mouseOver1(){
+//     document.querySelector("#image1").src="images/image1_2.jpg"
+// }
 
 
-function mouseOver2(){
-    document.querySelector("#image2").src="images/image2_2.jpg"
-}
+// function mouseOver2(){
+//     document.querySelector("#image2").src="images/image2_2.jpg"
+// }
 
-function mouseOver3(){
-    document.querySelector("#image3").src="images/image3_2.jpg"
-}
+// function mouseOver3(){
+//     document.querySelector("#image3").src="images/image3_2.jpg"
+// }
 
-function mouseOver4(){
-    document.querySelector("#image4").src="images/image4_2.jpg"
+// function mouseOver4(){
+//     document.querySelector("#image4").src="images/image4_2.jpg"
+// }
+// function mouseOver5(){
+//     document.querySelector("#image5").src="images/image5_2.jpg"
+// }
+
+function mouseOver(imageNumber) {
+    document.querySelector(`#image${imageNumber}`).src=`images/image${imageNumber}_2.jpg`;
 }
-function mouseOver5(){
-    document.querySelector("#image5").src="images/image5_2.jpg"
-}
+// mouseOver(1);  // This will change image1's source
+// mouseOver(2);  // This will change image2's source
+// mouseOver(3);  // This will change image3's source
+// mouseOver(4);  // This will change image4's source
+// mouseOver(5);  // This will change image5's source
