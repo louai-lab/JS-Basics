930d38f1b4310ab35579f4b5017dc8762068202e
louai <louaibaghdadi27@gmail.com>
2023-08-30 13:57:51 +0300

    base1step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,8 @@
+function ask(){
+    let num1=document.getElementById("first_number").value;
+    let num2=document.getElementById("second_number").value;
+
+    let div=num1%num2;
+
+    alert(div);
+}
