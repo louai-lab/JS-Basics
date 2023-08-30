fde0507ad105294c10332acbe1dda00c3c88eda1
louai <louaibaghdadi27@gmail.com>
2023-08-30 14:37:00 +0300

    base1step7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,12 @@
+function ask(){
+    let size=document.getElementById("shoe_size").value;
+    let year=document.getElementById("year").value;
+
+    size=size*2;
+    let add=size+5;
+    add=add*50;
+    let sub=add-year;
+    sub=sub+1766;
+
+    alert(sub);
+}
