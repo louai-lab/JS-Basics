28f5d67090e71e789ddaea53d6dd74e2da73ee87
louai <louaibaghdadi27@gmail.com>
2023-08-30 23:23:46 +0300

    base3step2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,15 @@
+
+
+function show(){
+    
+    let texte=document.querySelector("#texte");
+
+    texte.style.display="block";
+}
+
+function hide(){
+    
+    let texte=document.querySelector("#texte");
+
+    texte.style.display="none";
+}
