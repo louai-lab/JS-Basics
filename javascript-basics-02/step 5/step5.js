e1d199578dc01bd5f938b3402f270c6de1636e42
louai <louaibaghdadi27@gmail.com>
2023-08-30 21:43:06 +0300

    base2step5

---
+++ javascript-basics-02/step 5/step5.js
@@ -0,0 +1,21 @@
+
+
+function mouseOver1(){
+    document.querySelector("#image1").src="images/image1_2.jpg"
+}
+
+
+function mouseOver2(){
+    document.querySelector("#image2").src="images/image2_2.jpg"
+}
+
+function mouseOver3(){
+    document.querySelector("#image3").src="images/image3_2.jpg"
+}
+
+function mouseOver4(){
+    document.querySelector("#image4").src="images/image4_2.jpg"
+}
+function mouseOver5(){
+    document.querySelector("#image5").src="images/image5_2.jpg"
+}
