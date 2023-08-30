c8586a1b1cb1e970a54f081abaa29655faf2b718
louai <louaibaghdadi27@gmail.com>
2023-08-30 22:11:00 +0300

    base2step6

--- javascript-basics-02/step 6/step6.js
+++ javascript-basics-02/step 6/step6.js
@@ -1,5 +1,7 @@
-
-
 function mouseOver(imageNumber) {
     document.querySelector(`#image${imageNumber}`).src=`images/image${imageNumber}_2.jpg`;
+}
+
+function mouseOut(imageNumber) {
+    document.querySelector(`#image${imageNumber}`).src = `images/image${imageNumber}.jpg`;
 }
