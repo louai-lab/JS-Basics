0d7dd3eb9cda5e2687d18f3f150509a39eb66754
louai <louaibaghdadi27@gmail.com>
2023-08-31 14:20:11 +0300

    base2step3

--- javascript-basics-02/step 3/step3.js
+++ javascript-basics-02/step 3/step3.js
@@ -1,6 +1,19 @@
-function onChange(){
-    let div=document.querySelector("div");
-    let input=document.querySelector("#name");
+// function onChange(){
+//     let div=document.querySelector("div");
+//     let input=document.querySelector("#name");
 
-    div.innerHTML = input.value;
-}
+//     div.innerHTML = input.value;
+// }
+
+// let div=document.querySelector("#divv")
+
+let para=document.createElement("div");
+let input=document.querySelector("#name");
+
+document.body.appendChild(para);
+para.style.width="250px";
+para.style.height="250px";
+
+input.addEventListener("keyup",()=>{
+    para.innerHTML = input.value;
+})
