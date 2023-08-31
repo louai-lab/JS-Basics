972639c36905e6a464b82ddf67c15c9913636c9b
louai <louaibaghdadi27@gmail.com>
2023-08-31 15:04:42 +0300

    base3step1

--- javascript-basics-03/step 1/step1.js
+++ javascript-basics-03/step 1/step1.js
@@ -1,4 +1,13 @@
-function mouseOver(){
-    let img=document.querySelector("#image1");
-    img.style.border="3px solid red"
-}
+// function mouseOver(){
+//     let img=document.querySelector("#image1");
+//     img.style.border="3px solid red"
+// }
+
+
+let img=document.querySelector("#image1");
+
+console.log(img);
+
+img.addEventListener("mouseover",()=>{
+    img.style.border="3px solid red";
+})
