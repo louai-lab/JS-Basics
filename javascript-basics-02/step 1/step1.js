cc5d59025096b5193cb5f973a2ec028f80023731
louai <louaibaghdadi27@gmail.com>
2023-08-31 13:48:59 +0300

    base2step1

--- javascript-basics-02/step 1/step1.js
+++ javascript-basics-02/step 1/step1.js
@@ -1,5 +1,9 @@
-// var image=document.querySelector("#image1")
 
-function mouseOver(){
-    document.querySelector("#image1").src="images/image1_2.jpg"
-}
+
+let image=document.querySelector("#image1");
+
+image.addEventListener("mouseover",()=>{
+    image.src="images/image1_2.jpg";
+    
+})
+
