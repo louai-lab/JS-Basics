6c3c5ddbfbf1dc696e25049e67b7ee81089ddc36
louai <louaibaghdadi27@gmail.com>
2023-08-31 15:44:06 +0300

    base3step3

--- javascript-basics-03/step 3/step3.js
+++ javascript-basics-03/step 3/step3.js
@@ -1,9 +1,31 @@
 
 
-function changeColor(color) {
+// function changeColor(color) {
 
-    let paragraphe=document.getElementById("text");
+//     let paragraphe=document.getElementById("text");
     
-    paragraphe.style.color=color;
+//     paragraphe.style.color=color;
     
-    }
+//     }
+
+
+let colors=document.querySelectorAll(".color");
+let text=document.querySelector("#text")
+
+console.log(colors);
+console.log(text);
+
+colors.forEach(element =>{
+    element.addEventListener("click",(e)=>{
+        if(e.target.classList[1] ==="green"){
+            text.style.color="green";
+        }
+
+        else if(e.target.classList[1] ==="red"){
+            text.style.color="red";
+        }
+        else if(e.target.classList[1] ==="blue"){
+            text.style.color="blue"
+        }
+    })
+})
