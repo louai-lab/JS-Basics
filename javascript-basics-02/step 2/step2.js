5b5a2bbfc1446cf4b4d88a680c87d34c01ecf4a5
louai <louaibaghdadi27@gmail.com>
2023-08-31 13:50:42 +0300

    base2step2

--- javascript-basics-02/step 2/step2.js
+++ javascript-basics-02/step 2/step2.js
@@ -1,5 +1,11 @@
 
 
-function onBlur(){
-    alert("thank you for participating!")
-}
+// function onBlur(){
+//     alert("thank you for participating!")
+// }
+
+let input=document.querySelector("#name");
+
+input.addEventListener("blur",()=>{
+    alert("user onblur or addEventListener")
+})
