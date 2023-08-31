a3777337fbf2b8a5702a458e1a9e9d6f597d13ea
louai <louaibaghdadi27@gmail.com>
2023-08-31 14:51:43 +0300

    base2step6

--- javascript-basics-02/step 6/step6.js
+++ javascript-basics-02/step 6/step6.js
@@ -1,7 +1,19 @@
-function mouseOver(imageNumber) {
-    document.querySelector(`#image${imageNumber}`).src=`images/image${imageNumber}_2.jpg`;
-}
+// function mouseOver(imageNumber) {
+//     document.querySelector(`#image${imageNumber}`).src=`images/image${imageNumber}_2.jpg`;
+// }
 
-function mouseOut(imageNumber) {
-    document.querySelector(`#image${imageNumber}`).src = `images/image${imageNumber}.jpg`;
-}
+// function mouseOut(imageNumber) {
+//     document.querySelector(`#image${imageNumber}`).src = `images/image${imageNumber}.jpg`;
+// }
+
+let images=document.querySelectorAll("img");
+
+images.forEach(element => {
+    element.addEventListener("mouseover",()=>{
+        element.src=`images/${element.id}_2.jpg`;
+    });
+
+    element.addEventListener("mouseout",()=>{
+        element.src=`images/${element.id}.jpg`;
+    })
+});
