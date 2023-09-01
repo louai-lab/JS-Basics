69947db7549a429a5595762adae5bb0793435295
louai <louaibaghdadi27@gmail.com>
2023-09-01 10:10:15 +0300

    base1step6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,8 +1,21 @@
-function ask(){
-    let num1=document.getElementById("first_number").value;
-    let num2=document.getElementById("second_number").value;
+// function ask(){
+//     let num1=document.getElementById("first_number").value;
+//     let num2=document.getElementById("second_number").value;
 
-    let div=num1%num2;
+//     let div=num1%num2;
 
-    alert(div);
-}
+//     alert(div);
+// }
+
+
+let num1=document.querySelector("#first_number");
+let num2=document.querySelector("#second_number");
+let btn=document.querySelector("#validate");
+
+
+
+
+btn.addEventListener("click",()=>{
+    let mult=num1.value%num2.value;
+    alert(mult);
+})
