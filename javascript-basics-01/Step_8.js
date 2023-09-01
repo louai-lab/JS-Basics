34df8313ed16487292d4a17c552d441661b938a8
louai <louaibaghdadi27@gmail.com>
2023-09-01 10:15:27 +0300

    base1step8

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -1,10 +1,23 @@
-function ask(){
-    let age=document.getElementById("age").value;
+// function ask(){
+//     let age=document.getElementById("age").value;
 
+//     if(age > 18){
+//         alert("you are over 18");
+//     }
+//     else{
+//         alert("you are under 18");
+//     }
+// }
+
+let age=document.getElementById("age").value;
+let btn=document.querySelector("#validate");
+
+btn.addEventListener("click",()=>{
     if(age > 18){
         alert("you are over 18");
     }
     else{
         alert("you are under 18");
     }
-}
+})
+
