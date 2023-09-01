c13b7031444638d9f77bc3bdf64d84a4ad6d7cec
louai <louaibaghdadi27@gmail.com>
2023-09-01 10:13:05 +0300

    base1step7

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,7 +1,23 @@
-function ask(){
-    let size=document.getElementById("shoe_size").value;
-    let year=document.getElementById("year").value;
+// function ask(){
+//     let size=document.getElementById("shoe_size").value;
+//     let year=document.getElementById("year").value;
 
+//     size=size*2;
+//     let add=size+5;
+//     add=add*50;
+//     let sub=add-year;
+//     sub=sub+1766;
+
+//     alert(sub);
+// }
+
+
+let size=document.getElementById("shoe_size").value;
+let year=document.getElementById("year").value;
+let btn=document.querySelector("#validate");
+
+
+btn.addEventListener("click",()=>{
     size=size*2;
     let add=size+5;
     add=add*50;
@@ -9,4 +25,5 @@ function ask(){
     sub=sub+1766;
 
     alert(sub);
-}
+})
+
